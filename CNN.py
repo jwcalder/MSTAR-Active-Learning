695ce15bdc2f13ff80e973259c5dd4c9e3ee55d5
Jeff Calder <jcalder@umn.edu>
2021-09-06 12:53:37 -0500

    Added empty scripts

---
+++ CNN.py
@@ -0,0 +1,3 @@
+#Code for training CNN and saving CNN model and CNN features
+#Should also save the indices (or boolean mask) for the training set used to train the CNN
+
---
+++ graph_learning.py
@@ -0,0 +1,1 @@
+#Basic script to run Laplace learning on MSTAR data at various label rates over many trials
