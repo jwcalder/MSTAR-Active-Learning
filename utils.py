e755884008d1d0934a4f112adeeb2afb6b0d731b
Jeff Calder <jcalder@umn.edu>
2021-09-06 12:45:17 -0500

    Added utils.py

---
+++ utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def load_MSTAR():
+    """
+    Loads MSTAR Data
+
+    Returns:
+    header, fields, magnitude images, phase images
+    """
+
+    M = np.load('data/SAR10a.npz', allow_pickle=True)
+    hdr_a,fields,mag_a,phase_a = M['hdr'],M['fields'],M['mag'],M['phase']
+
+    M = np.load('data/SAR10b.npz', allow_pickle=True)
+    hdr_b,fields,mag_b,phase_b = M['hdr'],M['fields'],M['mag'],M['phase']
+
+    M = np.load('data/SAR10c.npz', allow_pickle=True)
+    hdr_c,fields,mag_c,phase_c = M['hdr'],M['fields'],M['mag'],M['phase']
+
+    hdr = np.concatenate((hdr_a,hdr_b,hdr_c))
+    mag = np.concatenate((mag_a,mag_b,mag_c))
+    phase = np.concatenate((phase_a,phase_b,phase_c))
+
+    return hdr, fields, mag, phase 
