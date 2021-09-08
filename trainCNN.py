d07883aff517c2a7b7a48f75013ad1da257647e1
Jeff Calder <jcalder@umn.edu>
2021-09-08 16:05:23 -0500

    Added trained CNN models

--- trainCNN.py
+++ trainCNN.py
@@ -1,5 +1,5 @@
 #Code for training CNN and saving CNN model
-#The variable train_fraction controls how much training data
+#The variable train_fraction_list controls how much training data
 #to use. The models are saved in the directory models/ 
 #with names indicating the amount of training data used. 
 #The indices of the training data are saved in models/ as well.
@@ -18,7 +18,7 @@ import utils
 import models
 
 #Training settings
-train_fraction = 1 #Fraction of full training data to use
+train_fraction_list = np.arange(0.05,1.05,0.05) #List of fractions of full training data to use
 cuda = True   #Use GPU acceleration
 batch_size = 150
 learning_rate = 1    #Learning rate
@@ -33,15 +33,18 @@ def train(model, device, data_train, target_train, optimizer, epoch, batch_size)
         data, target = data_train[idx:idx+batch_size], target_train[idx:idx+batch_size]
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % 10 == -1:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(target_train),
-                100. * batch_idx / int(len(data_train)/batch_size), loss.item()))
-        batch_idx += 1
+
+        #If by chance we get a batch size of 1, batchnormalization fails, so let's just skip
+        if data.shape[0] > 1:
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % 10 == -1:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(target_train),
+                    100. * batch_idx / int(len(data_train)/batch_size), loss.item()))
+            batch_idx += 1
 
 
 def test(model, device, data_test, target_test,name):
@@ -77,38 +80,49 @@ data = torch.from_numpy(data).float()
 labels, target_names = utils.targets_to_labels(hdr)
 labels = torch.from_numpy(labels).long()
 
-#Training and testing split
-full_train_mask, test_mask, train_idx = utils.train_test_split(hdr,train_fraction)
-data_train = data[train_idx,:,:,:]
-data_test = data[test_mask,:,:,:]
-target_train = labels[train_idx]
-target_test = labels[test_mask]
-
-#Randomly shuffle training data
-P = np.random.permutation(len(train_idx))
-data_train = data_train[P,:,:,:]
-target_train = target_train[P]
-
-#Cuda and optimizer/scheduler
+#Cuda and model to device
 use_cuda = cuda and torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 model = models.CNN().to(device)
-optimizer = optim.Adadelta(model.parameters(), lr=learning_rate)
-scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
 
-#Main training loop
-for epoch in range(1, epochs + 1):
-    train(model, device, data_train, target_train, optimizer, epoch, batch_size)
-    print('\nEpoch: %d'%epoch)
-    test_acc = test(model, device, data_test, target_test, 'Test ')
-    train_acc = test(model, device, data_train, target_train, 'Train')
-    scheduler.step()
+#Loop over different amounts of training data
+for train_fraction in train_fraction_list:
 
-#Save model
-torch.save(model, os.path.join('models','SAR10_CNN_%d.pt'%len(train_idx)))
+    #Training and testing split
+    full_train_mask, test_mask, train_idx = utils.train_test_split(hdr,train_fraction)
+    data_train = data[train_idx,:,:,:]
+    data_test = data[test_mask,:,:,:]
+    target_train = labels[train_idx]
+    target_test = labels[test_mask]
 
-#Save specific training indices used to train the CNN
-np.save(os.path.join('models',"SAR10_CNN_%d_training_indices"%len(train_idx)), train_idx)
+    #Randomly shuffle training data
+    P = np.random.permutation(len(train_idx))
+    data_train = data_train[P,:,:,:]
+    target_train = target_train[P]
+
+    #Reset parameters in the model
+    for layer in model.children():
+        if hasattr(layer, 'reset_parameters'):
+            layer.reset_parameters()
+
+    #Reset optimizer and scheduler
+    optimizer = optim.Adadelta(model.parameters(), lr=learning_rate)
+    scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+
+    #Main training loop
+    for epoch in range(1, epochs + 1):
+        train(model, device, data_train, target_train, optimizer, epoch, batch_size)
+        print('\nEpoch: %d'%epoch)
+        test_acc = test(model, device, data_test, target_test, 'Test ')
+        train_acc = test(model, device, data_train, target_train, 'Train')
+        scheduler.step()
+
+    
+    #Save model
+    torch.save(model, os.path.join('models','SAR10_CNN_%d.pt'%len(train_idx)))
+
+    #Save specific training indices used to train the CNN
+    np.save(os.path.join('models',"SAR10_CNN_%d_training_indices"%len(train_idx)), train_idx)
 
 
 
