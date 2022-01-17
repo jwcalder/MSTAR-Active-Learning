2758bae2c21a6a902ebd286a7caef597d3c3803e
Jeff Calder <jcalder@umn.edu>
2022-01-17 06:32:04 -0600

    Updated README

--- Python/display_images.py
+++ Python/display_images.py
@@ -3,24 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 hdr,fields,mag,phase = utils.load_MSTAR()
-n = mag.shape[0]
-R = np.random.permutation(n)
 
 numx,numy = 3,3
-fig, axs = plt.subplots(numx,numy,figsize=(15,15))
-fig.suptitle('Magnitude Images')
+fig1, axs1 = plt.subplots(numx,numy,figsize=(15,15))
+fig2, axs2 = plt.subplots(numx,numy,figsize=(15,15))
+fig1.suptitle('Magnitude Images')
+fig2.suptitle('Phase Images')
+n = mag.shape[0]
+R = np.random.permutation(n)
 for i in range(numx):
     for j in range(numy):
         img = mag[R[i],:,:]
-        axs[i,j].imshow(img,cmap='gray')
-        axs[i,j].set_title(hdr[R[i],0])
-
-fig, axs = plt.subplots(numx,numy,figsize=(15,15))
-fig.suptitle('Phase Images')
-for i in range(numx):
-    for j in range(numy):
+        axs1[i,j].imshow(img,cmap='gray')
+        axs1[i,j].set_title(hdr[R[i],0])
         img = phase[R[i],:,:]
-        axs[i,j].imshow(img,cmap='gray')
-        axs[i,j].set_title(hdr[R[i],0])
-
+        axs2[i,j].imshow(img,cmap='gray')
+        axs2[i,j].set_title(hdr[R[i],0])
 plt.show()
